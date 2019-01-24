cb35a3d910e66e687289801db4ab84a4daf3eec1
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-01-24 07:15:36 -0500

    Updates

---
+++ precache-manifest.4faaf03fb229f34203bce96939317296.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebc1501fcd2bf0e260de",
+    "url": "/text-animation/static/css/main.b598e15a.chunk.css"
+  },
+  {
+    "revision": "ebc1501fcd2bf0e260de",
+    "url": "/text-animation/static/js/main.ebc1501f.chunk.js"
+  },
+  {
+    "revision": "931bd213dd4c62fede94",
+    "url": "/text-animation/static/js/1.931bd213.chunk.js"
+  },
+  {
+    "revision": "65c19f7ed9ed326ec38c",
+    "url": "/text-animation/static/js/runtime~main.65c19f7e.js"
+  },
+  {
+    "revision": "d539151b5faf4d2842874fcc133bbad8",
+    "url": "/text-animation/index.html"
+  }
+];
