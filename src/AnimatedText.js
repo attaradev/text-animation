e59d3ea526338438068a3b6d2c348bc418fe0355
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-01-24 01:02:03 -0500

    update AnimatedText.js

--- src/AnimatedText.js
+++ src/AnimatedText.js
@@ -7,16 +7,45 @@ export const AnimatedText = ({
   overlayColor,
   children,
   ...props
-}) => (
-  <section className={classes.AnimatedTextContainer} {...props}>
-    <h1 style={{ color: textColor }} className={classes.AnimatedTextContent}>
-      {children}
-    </h1>
-    <h1
-      style={{ color: overlayColor }}
-      className={classes.AnimatedTextContentClone}
+}) => {
+  const containerElement = useRef(null);
+  const initialMousePosition = { x: 0, y: 0 };
+
+  const [mousePosition, setMousePosition] = useState(initialMousePosition);
+
+  const handleMouseMove = event => {
+    event = event.nativeEvent;
+    const element = containerElement.current;
+    const newX = (event.offsetX / element.clientWidth) * 100;
+    const newY = (event.offsetY / element.clientHeight) * 100;
+    const newMousePosition = {
+      x: newX,
+      y: newY
+    };
+
+    setMousePosition(newMousePosition);
+  };
+
+  const handleMouseOut = event => setMousePosition(initialMousePosition);
+
+  return (
+    <section
+      className={classes.AnimatedTextContainer}
+      style={{ "--maskX": mousePosition.x, "--maskY": mousePosition.y }}
+      onMouseMove={handleMouseMove}
+      onMouseOut={handleMouseOut}
+      ref={containerElement}
+      {...props}
     >
-      {children}
-    </h1>
-  </section>
-);
+      <h1 style={{ color: textColor }} className={classes.AnimatedTextContent}>
+        {children}
+      </h1>
+      <h1
+        style={{ color: overlayColor }}
+        className={classes.AnimatedTextContentClone}
+      >
+        {children}
+      </h1>
+    </section>
+  );
+};
