367a20b239d4ebc1be6a3c4adf361025c461ddaf
Mike Perry Yeboah Attara <mpyebattara@gmail.com>
2019-01-24 00:05:38 -0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
+import "./index.css";
+import { AnimatedText } from "./AnimatedText";
+
+const App = () => (
+  <div className="App">
+    <AnimatedText textColor="#cd122d" overlayColor="#fdc52c">
+      MIKE PERRY Y. ATTARA
+    </AnimatedText>
+  </div>
+);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
